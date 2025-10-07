5dbdc585099625ac8bf18bbc5062f3db4092dedb
Jakob Schröter <jakob.schroeter@knime.com>
2025-10-07 12:57:07 +0200

    KDS-434: export LinkButton
    KDS-434 (Button (formerly function button & button))

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,10 +1,12 @@
 // Export all components
 export { default as Button } from "./Button/Button.vue";
+export { default as LinkButton } from "./Button/LinkButton.vue";
 export { default as Icon } from "./Icon/Icon.vue";
 export { default as DataType } from "./Icon/DataType.vue";
 
 // Export component types
 export type * from "./Button/Button.vue";
+export type * from "./Button/LinkButton.vue";
 export type * from "./Icon/Icon.vue";
 export type * from "./Icon/DataType.vue";
 
