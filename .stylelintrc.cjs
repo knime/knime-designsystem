5e328b426be3aaef8e9bd43356c4137c741edaff
Jakob Schröter <jakob.schroeter@knime.com>
2025-10-13 15:59:22 +0200

    KDS-581: update eslint configs
    KDS-581 (Update eslint in knime-designsystem)

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,3 +1,14 @@
 module.exports = {
   extends: ["@knime/eslint-config/stylelint/vue"],
+  rules: {
+    "csstools/value-no-unknown-custom-properties": [
+      true,
+      {
+        importFrom: [
+          "packages/styles/dist/css/index.css",
+          "packages/styles/dist/tokens/css/_variables.css",
+        ],
+      },
+    ],
+  },
 };
