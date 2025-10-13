a1714adbffa6027cc27ac8fea96c296cb91e563e
Jakob Schröter <jakob.schroeter@knime.com>
2025-10-13 14:57:17 +0200

    KDS-434: remove styleint changes
    KDS-434 (Button (formerly function button & button))

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,18 +1,3 @@
 module.exports = {
-  extends: [
-    "@knime/eslint-config/stylelint/vue",
-    "stylelint-config-recess-order", // TODO move to @knime/eslint-config/stylelint
-  ],
-  plugins: ["stylelint-value-no-unknown-custom-properties"], // TODO move to @knime/eslint-config/stylelint
-  rules: {
-    "csstools/value-no-unknown-custom-properties": [
-      true,
-      {
-        importFrom: [
-          "packages/styles/dist/css/index.css",
-          "packages/styles/dist/tokens/css/_variables.css",
-        ],
-      },
-    ],
-  },
+  extends: "@knime/eslint-config/stylelint/vue",
 };
