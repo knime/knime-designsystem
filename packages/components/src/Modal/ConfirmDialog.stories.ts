6dab4eab23b7c5a5c032a56c8bde7d3b2ce187c3
Karl Glatz <karl@glatz.biz>
2025-11-06 20:38:31 +0100

    NXT-4191: more improvements
    NXT-4191 (Adapt useDialog of Hub and replace useConfirmDialog in spaceExploer)

--- packages/components/src/Modal/ConfirmDialog.stories.ts
+++ packages/components/src/Modal/ConfirmDialog.stories.ts
@@ -1,9 +1,14 @@
+import { h } from "vue";
 import type { Meta, StoryObj } from "@storybook/vue3-vite";
 
 import Button from "../Button/Button.vue";
 
 import ConfirmDialog from "./ConfirmDialog.vue";
-import { useConfirmDialog } from "./useConfirmDialog";
+import {
+  cancelButton,
+  confirmButton,
+  useConfirmDialog,
+} from "./useConfirmDialog";
 
 const meta: Meta<typeof ConfirmDialog> = {
   title: "Components/Modal/ConfirmDialog",
@@ -27,6 +32,25 @@ export const ConfirmDialogStory: Story = {
         show({
           title: "Sure?",
           message: "Are you sure?",
+          doNotAskAgain: {
+            label: "Do not ask again?",
+            title: "Some more info…",
+            helperText: "Can be changed in the settings.",
+          },
+          cancelButtons: [cancelButton("Nay")],
+          confirmButtons: [
+            {
+              type: "confirm",
+              label: "Maybe",
+              leadingIcon: "circle-question",
+              variant: "outlined",
+              autofocus: true,
+              customHandler: () => {
+                alert("Think again…");
+              },
+            },
+            confirmButton("Sure thing"),
+          ],
         });
       };
       return {
@@ -37,7 +61,44 @@ export const ConfirmDialogStory: Story = {
     template: `
     <Button
       label="Show confirm"
-      size="large"
+      variant="filled"
+      @click="showDialog"
+    />
+    <ConfirmDialog />
+  `,
+  }),
+  args: {},
+};
+
+export const ConfirmDialogWithCustomComponent: Story = {
+  render: (args) => ({
+    components: { ConfirmDialog, Button },
+    setup() {
+      const { show } = useConfirmDialog();
+
+      const showDialog = () => {
+        show({
+          title: "Can you see this?",
+          icon: "list-bulletpoint",
+          closedby: "any",
+          component: h("div", [
+            h("h3", "A Headline"),
+            h("p", [
+              "This is some ",
+              h("b", "bold"),
+              " text. Just supply a VNode as component.",
+            ]),
+          ]),
+        });
+      };
+      return {
+        args,
+        showDialog,
+      };
+    },
+    template: `
+    <Button
+      label="Component based content"
       variant="filled"
       @click="showDialog"
     />
