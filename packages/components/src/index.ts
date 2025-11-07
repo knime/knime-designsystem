dc32d58df5f4aa02cad0f1f7dc12fa19dec335d7
Karl Glatz <karl@glatz.biz>
2025-11-07 08:33:25 +0100

    NXT-4191: export modal to package; improve docs
    NXT-4191 (Adapt useDialog of Hub and replace useConfirmDialog in spaceExploer)

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -5,10 +5,15 @@ export { default as KdsCheckbox } from "./Checkbox/KdsCheckbox.vue";
 export { default as KdsIcon } from "./Icon/KdsIcon.vue";
 export { default as KdsDataType } from "./Icon/KdsDataType.vue";
 
+export { default as BaseModal } from "./Modal/BaseModal.vue";
+export { default as ConfirmDialog } from "./Modal/ConfirmDialog.vue";
+export * from "./Modal/useConfirmDialog";
+
 // Export component types
 export type * from "./Button/types";
 export type * from "./Checkbox/types";
 export type * from "./Icon/types";
+export type * from "./Modal/types";
 
 // Export utils
 export * from "./util";
