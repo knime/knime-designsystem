2ac68ef295c1fc25b4d6ab9153ab57ad6ed11eb5
Karl Glatz <karl@glatz.biz>
2025-11-14 11:00:55 +0100

    NXT-4191: add xlarge width to modal; improve types
    NXT-4191 (Adapt useDialog of Hub and replace useConfirmDialog in spaceExploer)

---
+++ packages/components/src/Modal/constants.ts
@@ -0,0 +1,10 @@
+export const closedByOptions = ["any", "closerequest", "none"] as const;
+export const widthSizes = [
+  "small",
+  "medium",
+  "large",
+  "xlarge",
+  "full",
+] as const;
+export const heightSizes = ["auto", "full"] as const;
+export const modalVariants = ["default", "plain"] as const;
