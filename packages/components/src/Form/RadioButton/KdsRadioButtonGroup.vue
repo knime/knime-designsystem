e10da5defc6c976c4f2f25973f29ac3ad4ce943f
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-01-13 11:19:45 +0100

    KDS-644: Enhance KdsRadioButtonGroup styles with font and color updates
    KDS-644 (RadiobuttonGroup)

--- packages/components/src/Form/RadioButton/KdsRadioButtonGroup.vue
+++ packages/components/src/Form/RadioButton/KdsRadioButtonGroup.vue
@@ -189,6 +189,8 @@ legend {
   align-items: center;
   min-height: var(--kds-dimension-component-height-0-75x);
   padding-bottom: var(--kds-spacing-input-label-spacing-bottom);
+  font: var(--kds-font-base-title-small-strong);
+  color: var(--kds-color-text-and-icon-neutral);
 }
 
 .icon-wrapper {
