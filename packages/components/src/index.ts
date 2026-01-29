b34fa189a7471fc9546c8cdc776073cc5afbb13e
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-01-29 13:11:27 +0100

    Fix: Readd missing export

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,6 @@
 // Export all components
 export { default as KdsIcon } from "./Icon/KdsIcon.vue";
+export { default as KdsDataType } from "./Icon/KdsDataType.vue";
 export { default as KdsModal } from "./Modal/KdsModal.vue";
 export { default as KdsModalLayout } from "./Modal/KdsModalLayout.vue";
 export { default as KdsDynamicModalProvider } from "./Modal/KdsDynamicModalProvider.vue";
