4c83f4392f9976f7e1e2e43b1025fd72ebf73b86
Christian Albrecht <christian.albrecht@knime.com>
2026-02-05 05:18:32 +0100

    adjust comment

--- packages/components/src/util/isDefinedAndNotEmpty.ts
+++ packages/components/src/util/isDefinedAndNotEmpty.ts
@@ -2,7 +2,8 @@ import { isDefined } from "@vueuse/core";
 
 /**
  * Checks if a value is defined and not an empty string.
- * @param value - The value to check
+ * @param obj - The object in which to check
+ * @param field - The field name to check in the object
  * @returns true if the value is defined and not an empty string
  */
 export function isDefinedAndNotEmpty(
