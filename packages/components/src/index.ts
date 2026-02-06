eb65b77a8519d013f6e5f39c0865555b6559b1ba
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-02-06 09:03:55 +0100

    KDS-588: fix rebase issue
    KDS-588 (InputField base component)

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,14 +2,10 @@
 export { default as KdsIcon } from "./Icon/KdsIcon.vue";
 export { default as KdsDataType } from "./Icon/KdsDataType.vue";
 export { default as KdsEmptyState } from "./EmptyState/KdsEmptyState.vue";
-export { default as KdsModal } from "./Modal/KdsModal.vue";
-export { default as KdsModalLayout } from "./Modal/KdsModalLayout.vue";
-export { default as KdsDynamicModalProvider } from "./Modal/KdsDynamicModalProvider.vue";
 export { default as KdsLoadingSpinner } from "./LoadingSpinner/KdsLoadingSpinner.vue";
 
 // Export component types
 export type * from "./Icon/types";
-export type * from "./Modal/types";
 export type { KdsEmptyStateProps } from "./EmptyState/types";
 export type * from "./forms/Input/types";
 
