28d3e5236df5a72c0c5ca8cc21168387e429dd70
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-02-12 17:19:06 +0100

    KDS-468: Add KdsTextarea component for multi-line text input (#186)
    KDS-468 (Textarea)

--- packages/components/src/forms/index.ts
+++ packages/components/src/forms/index.ts
@@ -9,6 +9,7 @@ export { default as KdsTextInput } from "./inputs/TextInput/KdsTextInput.vue";
 export { default as KdsNumberInput } from "./inputs/NumberInput/KdsNumberInput.vue";
 export { default as KdsPatternInput } from "./inputs/PatternInput/KdsPatternInput.vue";
 export { default as KdsSearchInput } from "./inputs/SearchInput/KdsSearchInput.vue";
+export { default as KdsTextarea } from "./inputs/Textarea/KdsTextarea.vue";
 
 // Export form component types
 export type {
@@ -32,4 +33,5 @@ export type {
   KdsNumberInputProps,
   KdsPatternInputProps,
   KdsSearchInputProps,
+  KdsTextareaProps,
 } from "./inputs/types";
