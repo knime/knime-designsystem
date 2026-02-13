320d5e464e8fa563145c0d669051a8e536398289
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-02-13 08:22:13 +0100

    KDS-718: KDS-718: Update exports for KdsColorSwatch and KdsAvatar components; modify KdsAvatar stories for improved testing
    KDS-718 (ListItem)

--- packages/components/src/accessories/index.ts
+++ packages/components/src/accessories/index.ts
@@ -1,6 +1,10 @@
+export { default as KdsColorSwatch } from "./ColorSwatch/KdsColorSwatch.vue";
+export { default as KdsAvatar } from "./Avatar/KdsAvatar.vue";
 export { default as KdsIcon } from "./Icon/KdsIcon.vue";
 export { default as KdsDataType } from "./Icon/KdsDataType.vue";
 export { default as KdsLoadingSpinner } from "./LoadingSpinner/KdsLoadingSpinner.vue";
 
+export type * from "./ColorSwatch/types";
+export type * from "./Avatar/types";
 export type * from "./Icon/types";
 export type * from "./LoadingSpinner/types";
