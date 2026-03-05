e1b2e52320bcebc37b2f8667f728a25bf8f89750
mitri24 <mireio.trinley@knime.com>
2026-03-05 14:00:12 +0100

    KDS-509: Add loading skeleton
    KDS-509 ()

---
+++ packages/components/src/accessories/LoadingSkeleton/enums.ts
@@ -0,0 +1,8 @@
+export const kdsInlineMessageVariant = {
+  WARNING: "warning",
+  ERROR: "error",
+  SUCCESS: "success",
+  INFO: "info",
+} as const;
+
+export const kdsInlineMessageVariants = Object.values(kdsInlineMessageVariant);
