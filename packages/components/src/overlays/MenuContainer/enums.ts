b3cb7cfb0b6bf5a238e07322f367ff040015d818
Vitomir Budimir <vitomir.budimir@knime.com>
2026-03-12 15:27:06 +0100

    KDS-532: Remove enum forwarding
    KDS-532 (MenuContainer)

--- packages/components/src/overlays/MenuContainer/enums.ts
+++
@@ -1,7 +0,0 @@
-import { kdsPopoverPlacement } from "../../overlays/Popover";
-
-export const kdsMenuContainerPlacement = kdsPopoverPlacement;
-
-export const kdsMenuContainerPlacements = Object.values(
-  kdsMenuContainerPlacement,
-);
