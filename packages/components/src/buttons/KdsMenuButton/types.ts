f0de9e45060f5be257480634face690034865561
Vitomir Budimir <vitomir.budimir@knime.com>
2026-03-12 14:44:17 +0100

    KDS-532: Remove type leftover
    KDS-532 (MenuContainer)

--- packages/components/src/buttons/KdsMenuButton/types.ts
+++ packages/components/src/buttons/KdsMenuButton/types.ts
@@ -1,7 +1,4 @@
 import type { KdsMenuContainerProps } from "../../overlays/MenuContainer/types";
-import type { KdsPopoverProps } from "../../overlays/Popover";
 import type { KdsToggleButtonProps } from "../KdsToggleButton";
 
-export type KdsMenuButtonProps = KdsToggleButtonProps &
-  KdsMenuContainerProps &
-  Pick<KdsPopoverProps, "placement">;
+export type KdsMenuButtonProps = KdsToggleButtonProps & KdsMenuContainerProps;
