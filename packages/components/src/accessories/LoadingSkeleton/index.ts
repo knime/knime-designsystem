6a0e612f4e56a9ab8b9ebec4a48f0a7325409dd9
mitri24 <mireio.trinley@knime.com>
2026-03-12 15:02:11 +0100

    KDS-509: KdsLoadingSkeleton
    KDS-509 ()

--- packages/components/src/accessories/LoadingSkeleton/index.ts
+++ packages/components/src/accessories/LoadingSkeleton/index.ts
@@ -1,12 +1,2 @@
 export { default as KdsLoadingSkeleton } from "./KdsLoadingSkeleton.vue";
-export {
-  kdsLoadingSkeletonButtonPreset,
-  kdsLoadingSkeletonButtonPresets,
-  kdsLoadingSkeletonCardPreset,
-  kdsLoadingSkeletonCardPresets,
-  kdsLoadingSkeletonIconPreset,
-  kdsLoadingSkeletonIconPresets,
-  kdsLoadingSkeletonTextPreset,
-  kdsLoadingSkeletonTextPresets,
-} from "./enums";
 export type * from "./types";
