ad771cc420f69088735f03bf842987aa7c4e6f32
Rainer Schmöger <rainer.schmoeger@tngtech.com>
2026-03-13 09:56:56 +0100

    KDS-532: fix space issue
    KDS-532 ()

--- packages/components/src/forms/_helper/List/ListContainer/KdsListContainer.vue
+++ packages/components/src/forms/_helper/List/ListContainer/KdsListContainer.vue
@@ -179,6 +179,13 @@ const handleKeydown = (event: KeyboardEvent) => {
       ) {
         break;
       }
+      if (
+        event.target instanceof HTMLElement &&
+        ["BUTTON"].includes(event.target.tagName) &&
+        event.target.ariaExpanded === "false"
+      ) {
+        break;
+      }
       if (activeId.value) {
         emit("itemClick", toOptionId(activeId.value));
         event.preventDefault();
